9cd279558ccf655bceefd936d916fb7ce10bdb28
149203 <149203@gmail.com>
2020-05-25 09:55:26 -0700

    add orderBy

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./style/master.scss"; // applies global scss styles
 import { uiData } from "./data/ui";
 import FunctionUI from "./components/FunctionUI";
+import orderBy from "lodash/orderBy";
 
 export default class App extends React.Component {
    constructor() {
@@ -10,10 +11,25 @@ export default class App extends React.Component {
    }
 
    render() {
+      const orderedData = orderBy(uiData, "name", "desc");
+
+      const getFunctionsNum = () => {
+         console.log(uiData);
+         return 6;
+      };
+
       return (
          <div className="container">
             <div className="row">
-               {uiData.map((functionUI) => {
+               <div class="col-12">
+                  <h1 class="d-flex justify-content-center">
+                     JavaScript Functions: Basic
+                  </h1>
+                  <p class="text-center lead mb-4">
+                     {getFunctionsNum()}&nbsp;functions documented
+                  </p>
+               </div>
+               {orderedData.map((functionUI) => {
                   return (
                      <FunctionUI
                         name={functionUI.name}
