0ad70b3f23a7c656b28a0a3be4f3eb59812bbf84
149203 <149203@gmail.com>
2020-05-25 14:51:33 -0700

    Revert "add all component functionality"
    This reverts commit 93b2bc291ab288a3f77bc4fde2abc151e3c5fb2e.

--- src/App.js
+++ src/App.js
@@ -3,86 +3,38 @@ import "./style/master.scss"; // applies global scss styles
 import { uiData } from "./data/ui";
 import FunctionUI from "./components/FunctionUI";
 import orderBy from "lodash/orderBy";
-import find from "lodash/find";
 
 export default class App extends React.Component {
-   constructor(props) {
-      super(props);
+   constructor() {
+      super();
       console.log(uiData);
-      const stateData = uiData.map((component) => {
-         return {
-            name: component.name,
-            isResultOpen: false,
-            result: null,
-            isCodeOpen: false,
-         };
-      });
-      this.state = { components: stateData };
-      this.showCode = this.showCode.bind(this);
-      this.showResult = this.showResult.bind(this);
-   }
-
-   showCode(name) {
-      const components = [...this.state.components];
-      const thisObjInState = find(
-         components,
-         (component) => component.name === name
-      );
-      thisObjInState.isCodeOpen === false
-         ? (thisObjInState.isCodeOpen = true)
-         : (thisObjInState.isCodeOpen = false);
-
-      this.setState({ components });
-   }
-
-   showResult(name, result) {
-      const components = [...this.state.components];
-      const thisObjInState = find(
-         components,
-         (component) => component.name === name
-      );
-      thisObjInState.isResultOpen = true;
-      thisObjInState.result = result;
-      console.log(result);
-
-      this.setState({ components });
    }
 
    render() {
       const orderedData = orderBy(uiData, "name", "desc");
 
       const getFunctionsNum = () => {
+         console.log(uiData);
          return 6;
       };
 
       return (
          <div className="container">
             <div className="row">
-               <div className="col-12">
-                  <h1 className="d-flex justify-content-center">
+               <div class="col-12">
+                  <h1 class="d-flex justify-content-center">
                      JavaScript Functions: Basic
                   </h1>
-                  <p className="text-center lead mb-4">
+                  <p class="text-center lead mb-4">
                      {getFunctionsNum()}&nbsp;functions documented
                   </p>
                </div>
                {orderedData.map((functionUI) => {
-                  const { name, desc, inputs, order } = functionUI;
-                  const thisObjInState = find(
-                     this.state.components,
-                     (component) => component.name === name
-                  );
                   return (
                      <FunctionUI
-                        name={name}
-                        desc={desc}
-                        inputs={inputs}
-                        key={order}
-                        isResultOpen={thisObjInState.isResultOpen}
-                        isCodeOpen={thisObjInState.isCodeOpen}
-                        showCode={this.showCode}
-                        showResult={this.showResult}
-                        result={thisObjInState.result}
+                        name={functionUI.name}
+                        desc={functionUI.desc}
+                        inputs={functionUI.inputs}
                      />
                   );
                })}
--- src/components/FunctionUI.js
+++ src/components/FunctionUI.js
@@ -1,61 +1,31 @@
 import React from "react";
-import utils from "../utils/functions";
-import convertDataType from "../utils/convertDataType";
 
 export default function FunctionUI(props) {
-   const renderInputs = (num, name) => {
+   const renderInputs = (num) => {
       const inputs = [];
       for (let i = 0; i < num; i++) {
          inputs.push(
-            <input
-               type="text"
-               className="form-control inline-action"
-               key={i}
-               id={`input-${name}-${i}`}
-            />
+            <input type="text" className="form-control inline-action" />
          );
       }
       return inputs;
    };
 
-   const getUserInput = (e, inputs, name) => {
-      const inputValues = [];
-      for (let i = 0; i < inputs; i++) {
-         const element = document.getElementById(`input-${name}-${i}`);
-         const value = element.value;
-         const convertedValue = convertDataType(value);
-         inputValues.push(convertedValue);
-      }
-      console.log(inputValues);
-      const result = utils[name](...inputValues);
-      console.log(result);
-      // update state
-      props.showResult(name, JSON.stringify(result));
-   };
-
    return (
       <div className="col-12 col-lg-8 offset-lg-2 mb-5">
-         <p className="name" onClick={(e) => props.showCode(props.name)}>
+         <p className="name">
             <b>{props.name}</b>&nbsp;-&nbsp;{props.desc}
          </p>
-         {props.isCodeOpen && (
-            <pre>
-               <code>{String(utils[props.name])}</code>
-            </pre>
-         )}
+         <pre style={{ display: "none" }}>
+            <code></code>
+         </pre>
          <div className="actions float-right">
-            {renderInputs(props.inputs, props.name)}
-            <button
-               className="btn btn-primary inline-action"
-               onClick={(e) => getUserInput(e, props.inputs, props.name)}
-            >
-               Run
-            </button>
+            {renderInputs(props.inputs)}
+            <button className="btn btn-primary inline-action">Run</button>
          </div>
          <div className="clearfix mb-3"></div>
-         {props.isResultOpen && (
-            <div className="alert alert-primary">{props.result}</div>
-         )}
+         <div className="alert alert-primary" style={{ display: "none" }}></div>
+         <div className="alert alert-danger" style={{ display: "none" }}></div>
       </div>
    );
 }
